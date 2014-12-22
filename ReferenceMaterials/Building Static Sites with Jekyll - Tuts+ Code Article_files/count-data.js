1b21b7bd91f32cd4abb48d4898fda14d07729b0e
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2014-12-22 12:10:10 -0800

    reference materials for plane ride home

---
+++ ReferenceMaterials/Building Static Sites with Jekyll - Tuts+ Code Article_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"22211 http:\/\/net.tutsplus.com\/?p=22211","comments":47}]});
+}
