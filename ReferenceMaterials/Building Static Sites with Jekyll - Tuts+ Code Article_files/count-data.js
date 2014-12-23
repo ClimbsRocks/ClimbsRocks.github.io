2f1d045a82af5576f13054fa970f084e8a75699b
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2014-12-23 00:41:17 -0500

    renaming reference materials

--- ReferenceMaterials/Building Static Sites with Jekyll - Tuts+ Code Article_files/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"22211 http:\/\/net.tutsplus.com\/?p=22211","comments":47}]});
-}
